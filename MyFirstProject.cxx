42146d014c9c72d6b1daedbb13f9194317ddaa35
Caleb <73370112+CHemmestad@users.noreply.github.com>
2021-08-25 13:28:40 -0500

    Add files via upload

---
+++ MyFirstProject.cxx
@@ -0,0 +1,20 @@
+/*
+ Your name
+	C++ 2021
+	Due: 
+	Lab number 1 Caleb Hemmestad
+	My first Project
+ */
+
+
+#include <iostream>
+
+using namespace std ;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello World! Red \n" ;
+	
+	return 0;
+}
+
